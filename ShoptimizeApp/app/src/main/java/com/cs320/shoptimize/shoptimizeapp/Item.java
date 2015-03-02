129841b8ade4f088c6a89076859013f6724b8a56
Gabe M <gmarkv10@gmail.com>
2015-03-02 15:45:42 -0500

    Item class

---
+++ ShoptimizeApp/app/src/main/java/com/cs320/shoptimize/shoptimizeapp/Item.java
@@ -0,0 +1,32 @@
+package com.cs320.shoptimize.shoptimizeapp;
+
+/**
+ * Created by Gabe Markarian on 3/2/2015.
+ */
+public class Item {
+
+    private String name;
+    private boolean coupon;
+
+
+
+
+    public Item(String name, boolean coupon){
+        this.name = name;
+        this.coupon = coupon;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public boolean getCoupon(){
+        return coupon;
+    }
+
+    public String getCouponAsStr() {
+        if (coupon) return "coupon found!";
+        else return "no coupon.";
+    }
+
+}
