0d3639fd9548693c7c2edad3dadc38e71ec27989
Gabe M <gmarkv10@gmail.com>
2015-03-02 21:47:38 -0500

    You can add list items now!

--- ShoptimizeApp/app/build/generated/source/r/debug/com/cs320/shoptimize/shoptimizeapp/R.java
+++ ShoptimizeApp/app/build/generated/source/r/debug/com/cs320/shoptimize/shoptimizeapp/R.java
@@ -1314,14 +1314,16 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f09002e;
         public static final int action_mode_bar_stub=0x7f09002d;
         public static final int action_mode_close_button=0x7f090020;
-        public static final int action_settings=0x7f090044;
+        public static final int action_settings=0x7f090046;
         public static final int activity_chooser_view_content=0x7f090021;
+        public static final int add_item_button=0x7f090042;
+        public static final int add_item_field=0x7f090041;
         public static final int always=0x7f090016;
         public static final int beginning=0x7f090013;
         public static final int checkbox=0x7f090029;
         public static final int collapseActionView=0x7f090017;
-        public static final int coupon=0x7f090043;
-        public static final int couponCheck=0x7f090042;
+        public static final int coupon=0x7f090045;
+        public static final int couponCheck=0x7f090044;
         public static final int decor_content_parent=0x7f09002f;
         public static final int default_activity_button=0x7f090024;
         public static final int dialog=0x7f09001b;
@@ -1337,7 +1339,7 @@ containing a value of this type.
         public static final int ifRoom=0x7f090018;
         public static final int image=0x7f090023;
         public static final int listMode=0x7f090009;
-        public static final int listView=0x7f090041;
+        public static final int listView=0x7f090043;
         public static final int list_item=0x7f090025;
         public static final int middle=0x7f090015;
         public static final int never=0x7f090019;
@@ -1428,8 +1430,10 @@ containing a value of this type.
         public static final int abc_toolbar_collapse_description=0x7f0b0010;
         public static final int action_settings=0x7f0b0011;
         public static final int app_name=0x7f0b0012;
-        public static final int hello_world=0x7f0b0013;
-        public static final int store=0x7f0b0014;
+        public static final int button_txt=0x7f0b0013;
+        public static final int hello_world=0x7f0b0014;
+        public static final int hint=0x7f0b0015;
+        public static final int store=0x7f0b0016;
     }
     public static final class style {
         /**  Customize your theme here. 
--- ShoptimizeApp/app/src/main/java/com/cs320/shoptimize/shoptimizeapp/MainActivity.java
+++ ShoptimizeApp/app/src/main/java/com/cs320/shoptimize/shoptimizeapp/MainActivity.java
@@ -3,15 +3,20 @@ package com.cs320.shoptimize.shoptimizeapp;
 import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +25,7 @@ import java.util.List;
 public class MainActivity extends ActionBarActivity {
 
     List<Item> items = new ArrayList<Item>();
+    EditText addField;
 
     ListView lv;
 
@@ -28,9 +34,35 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        addField = (EditText) findViewById(R.id.add_item_field);
+        final Button addBtn = (Button) findViewById(R.id.add_item_button);
         lv = (ListView) findViewById(R.id.listView);
         populateSL();
+
+        addBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                items.add(new Item(addField.getText().toString(), false));
+                Toast.makeText(getApplicationContext(), "Item added", Toast.LENGTH_SHORT).show();
+                addField.setText("");
+            }
+        });
+        addField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                addBtn.setEnabled(!addField.getText().toString().trim().isEmpty());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         ArrayAdapter<Item> adapter = new ItemListAdapter(this, R.layout.listview_item, items );
         lv.setAdapter(adapter);
@@ -63,8 +95,6 @@ public class MainActivity extends ActionBarActivity {
         addItem("Butter", false);
         addItem("Salt", false);
         addItem("Sardines", false);
-        addItem("Condoms", false);
-        addItem("Hatchet", false);
     }
 
     private void addItem(String name, boolean coupon){
